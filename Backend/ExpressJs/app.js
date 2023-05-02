bf8b8db28ca02d5fe624358758607ebe256a1e9f
abrar0786 <theibrar1000@gmail.com>
2023-05-02 16:32:16 +0500

    added functionality of how create api and post api functionality

--- Backend/ExpressJs/app.js
+++ Backend/ExpressJs/app.js
@@ -1,20 +1,51 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("welcome to express development with node Js development ");
-  console.log("good");
+  res.send({
+    message: "welcome to express development with node Js development ",
+    app: "Node",
+  });
 });
 
-app.get("/home", (req, res) => {
-  res.send("welcome to home page of express development");
+const json = fs.readFileSync(`${__dirname}/tours.json`);
+
+const tours = JSON.parse(json);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/tours.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tours: newTour,
+      },
+    });
+  });
+  // res.send("done status");
 });
 
 app.get("/about", (req, res) => {
-  res.send("welcome to about page of express development");
+  res.status(200).send("welcome to about page of express development");
 });
 
 const port = 5000;
 app.listen(port, () => {
-  console.log(`welcome to express port ${port}`);
+  console.log(`Server stated in express at port ${port}`);
 });
